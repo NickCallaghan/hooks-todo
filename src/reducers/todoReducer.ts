a1a2e01c621e25e65ca5b0a5eac3762307569d4a
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-04-30 12:10:07 +0100

    convert reducer to ts

--- src/reducers/todoReducer.ts
+++ src/reducers/todoReducer.ts
@@ -1,19 +1,26 @@
 import { v4 as uuidv4 } from "uuid";
+import { Todos } from "../types/types";
 
-const todoReducer = (todos, action) => {
+type Actions =
+  | { type: "ADD"; task: String }
+  | { type: "TOGGLE"; id: String }
+  | { type: "EDIT"; task: String; id: String }
+  | { type: "REMOVE"; id: String };
+
+const todoReducer = (todos: Todos, action: Actions) => {
   switch (action.type) {
     case "ADD":
       return [...todos, { id: uuidv4(), task: action.task, complete: false }];
     case "TOGGLE":
-      return todos.map(t =>
+      return todos.map((t) =>
         action.id !== t.id ? t : { ...t, complete: !t.complete }
       );
     case "EDIT":
-      return todos.map(t =>
+      return todos.map((t) =>
         t.id !== action.id ? t : { ...t, task: action.task }
       );
     case "REMOVE":
-      return todos.filter(t => action.id !== t.id);
+      return todos.filter((t) => action.id !== t.id);
     default:
       return todos;
   }
---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+// { id: 1, task: "Learn About React Hooks", complete: false },
+
+export type Todo = {
+  id: String;
+  task: String;
+  complete: Boolean;
+};
+
+export type Todos = [Todo];
